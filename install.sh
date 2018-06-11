81e203337cdc5c6510f2c09af51dba2fc81edaf0
twinbird <ixa2063@gmail.com>
2018-06-11 17:25:16 +0900

    add vim table mode

--- install.sh
+++ install.sh
@@ -13,6 +13,7 @@ ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
 
 # download vim plugin
 curl -sL https://github.com/kien/ctrlp.vim/archive/1.79.tar.gz | tar xz -C .vim/pack/mypackage/start/
+curl -sL https://github.com/dhruvasagar/vim-table-mode/archive/v4.6.7.tar.gz | tar xz -C .vim/pack/mypackage/start/
 
 # change ignore files
 git update-index --skip-worktree .hbp
