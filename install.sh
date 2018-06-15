7d41d4c91f7ca09f7e4bf261ef43f7aa8027b9fe
twinbird <ixa2063@gmail.com>
2018-06-15 23:01:15 +0900

    change to simple

--- install.sh
+++ install.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-# setup for ubuntu
-sudo apt install tree git vim tmux ruby ruby-dev libsqlite3-dev libxml2 libxml2-dev libxslt-dev ruby-bundler zlib1g-dev build-essential libssl-dev libreadline-dev
-sudo gem rails
-
 # links to dotfiles
 ln -s $HOME/dotfiles/.dircolors $HOME/.dircolors
 ln -s $HOME/dotfiles/.hbp $HOME/.hbp
@@ -12,6 +8,7 @@ ln -s $HOME/dotfiles/.vim $HOME/.vim
 ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
 
 # download vim plugin
+mkdir -p .vim/pack/mypackage/start/
 curl -sL https://github.com/kien/ctrlp.vim/archive/1.79.tar.gz | tar xz -C .vim/pack/mypackage/start/
 curl -sL https://github.com/dhruvasagar/vim-table-mode/archive/v4.6.7.tar.gz | tar xz -C .vim/pack/mypackage/start/
 
