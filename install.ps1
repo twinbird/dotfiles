2052ed6297f8c756d828a864227cc932b32ee44e
twinbird <ixa2063@gmail.com>
2018-10-26 13:29:55 +0900

    add install file for windows

---
+++ install.ps1
@@ -0,0 +1,31 @@
+# Replace vim files
+Remove-Item $HOME/_vimrc -Force
+Remove-Item $HOME/_gvimrc -Force
+Remove-Item $HOME/vimfiles -Recurse -Force
+
+Copy-Item $HOME/dotfiles/.vimrc $HOME/_vimrc
+Copy-Item $HOME/dotfiles/_gvimrc $HOME/_gvimrc
+Copy-Item $HOME/dotfiles/.vim $HOME/vimfiles -Recurse
+
+# Enable tls1.2
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
+
+# Download vim plugin
+# package location
+$package_path="vimfiles/pack/mypackage/start"
+New-Item $package_path -type directory -Force
+
+# ctrlp
+invoke-webrequest -uri https://github.com/kien/ctrlp.vim/archive/1.79.tar.gz -outfile $HOME"/"$package_path"/ctrlp.tar.gz"
+tar xfz $HOME"/"$package_path"/ctrlp.tar.gz" -C $HOME"/"$package_path
+
+# vim-table-mode
+invoke-webrequest -uri https://github.com/dhruvasagar/vim-table-mode/archive/v4.6.7.tar.gz -outfile $HOME"/"$package_path"/vim-table-mode.tar.gz"
+tar xfz $HOME"/"$package_path"/vim-table-mode.tar.gz" -C $HOME"/"$package_path
+
+# nerdtree
+invoke-webrequest -uri https://github.com/scrooloose/nerdtree/archive/5.0.0.tar.gz -outfile $HOME"/"$package_path"/nerdtree.tar.gz"
+tar xfz $HOME"/"$package_path"/nerdtree.tar.gz" -C $HOME"/"$package_path
+
+# remove tar files
+remove-item $package_path"/*.tar.gz"
