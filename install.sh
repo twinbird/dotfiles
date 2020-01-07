5343da8032465ded10cc17367b8cdca1ef64f4ad
twinbird <ixa2063@gmail>
2020-01-07 14:55:48 +0900

    add go-imports and comment out windows vimfiles

--- install.sh
+++ install.sh
@@ -12,5 +12,6 @@ cd $HOME/.vim/pack/mypackage/start
 
 git clone https://github.com/kien/ctrlp.vim.git
 git clone https://github.com/dhruvasagar/vim-table-mode.git
+git clone https://github.com/mattn/vim-goimports.git
 
 cd -
