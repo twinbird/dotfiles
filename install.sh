a44fb4edb39a5fac277b6d9a57875901a9cacc98
twinbird <ixa2063@gmail.com>
2022-08-26 15:22:04 +0900

    improve

--- install.sh
+++ install.sh
@@ -5,14 +5,8 @@ ln -s $HOME/dotfiles/.dircolors $HOME/.dircolors
 ln -s $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
 ln -s $HOME/dotfiles/_vimrc $HOME/.vimrc
 
-# bash configuration
-cat <<'EOS' >> ~/.bash_profile
-stty werase undef
-bind '"\C-w": unix-filename-rubout'
+# bash configuration notes
+# write to .profile
 
-function _compreply_ssh(){
-  COMPREPLY=(`cat ~/.ssh/config | grep -i -e '^host' | cut -d " " -f 2 | grep -E "$2"`)
-}
-complete -F _compreply_ssh ssh scp sftp
-EOS
-
+#stty werase undef
+#bind '"\C-w": unix-filename-rubout'
