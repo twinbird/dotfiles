22fcbaa5c950c40437746ed52959d235bd4afc2a
twinbird <ixa2063@gmail.com>
2022-11-15 04:20:59 +0900

    screenの設定を追加

--- install.sh
+++ install.sh
@@ -2,11 +2,18 @@
 
 # links to dotfiles
 ln -s $HOME/dotfiles/.dircolors $HOME/.dircolors
-ln -s $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
+#ln -s $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
+ln -s $HOME/dotfiles/.screenrc $HOME/.screenrc
 ln -s $HOME/dotfiles/_vimrc $HOME/.vimrc
 
 # bash configuration notes
 # write to .profile
 
-#stty werase undef
+# stty werase undef
 #bind '"\C-w": unix-filename-rubout'
+
+# for WSL2(write to .bashrc)
+#export SCREENDIR=$HOME/.screendir
+#function open() {
+#  cmd.exe /c start $(wslpath -w $1)
+#}
